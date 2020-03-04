8dfaa836b1c544729825f26fb9768117d832c644
nguyen hang <nguyenthihangtn1996@gmail.com>
2020-03-05 00:01:50 +0700

    Update new

---
+++ blog/blog/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from .views import index, blog, post
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blog),
+    path('post/', post),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,  documents_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,  documents_root = settings.MEDIA_ROOT)
