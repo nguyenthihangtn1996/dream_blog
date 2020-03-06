13001ee34aa775a7a9390f24fb75786c369f7d0b
hang <nguyenthihangtn1996.com>
2020-03-06 08:09:03 +0700

    Update code

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from maketing.models import Signup
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     queryset = Post.objects.filter(feature=True)
@@ -19,14 +20,27 @@ def index(request):
     return render(request, 'index.html', context )
 
 def blog(request):
-    queryset = Post.objects.filter(feature=True)
-    latest = Post.objects.order_by('-timestamp')[0:3]
+    queryset = Post.objects.all()
+    paginator = Paginator(queryset, 4)
+    page_request_var = 'page'
+    recently = Post.objects.order_by('-timestamp')[0:3]
+
+    page = request.GET.get(page_request_var)
+
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
 
     context = {
-        'object_list' : queryset,
-        'latest': latest
+        'object_list' : paginated_queryset,
+        'page_request_var' : page_request_var,
+        'recently': recently
     }
     return render(request, 'blog.html', context)
 
-def post(request):
-    return render(request, 'blog.html', {})
+def post(request, id):
+
+    return render(request, 'post.html', {})
